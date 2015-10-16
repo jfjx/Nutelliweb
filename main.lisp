b885def60d7e5544a4a7b5f4c0fdb2dce0157ce3
CH.Idea <sbw228@gmail.com>
2015-10-16 12:11:09 +0900

    main.lisp maintenance

--- main.lisp
+++ main.lisp
@@ -1,22 +1,20 @@
-;Load KM
-(load #P"lib/km-2-5-33.fasl") ; Load coompiled KM
-;(loop for lib in '(
-;   		   #P"lib/km-2-5-33.lisp"
-;		   )
-;   do (load lib))
+;Load dependencies
+(loop for lib in '(
+   		   #P"lib/km-2-5-33.fasl" ; Compiled KM
+		   #P"webapp/nutelliweb.asd" ; Webserver
+		   )
+   do (load lib))
 
-;(compile-file #P"lib/km-2-5-33.lisp")					;compile KM for faster process
+;(compile-file #P"lib/km-2-5-33.lisp")  ;compile KM for faster process
 
 
 (ql:quickload :caveman2)
 
 ; This "Nutrition" KM is mal-modeled and needed to be replaced.
-					;(untracekm) ;stop trace
+;(untracekm) ;stop trace
 (untracekm)
 (km '(load-kb "kb/Nutelliweb.kb")) ;load a chemical nutrition kb
 
 ;Load and start web server
-;(caveman2:make-project #P"webapp" :name "nutelliweb") ;make a new skeleton
-(load "webapp/nutelliweb.asd")
 (asdf:operate 'asdf:load-op 'nutelliweb) ;load
 (nutelliweb:start :port 9099) ;start
