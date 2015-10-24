f29d83844f62e4bc3eadc8bcbd08cc12978577db
CH.Idea <sbw228@gmail.com>
2015-10-25 04:22:34 +0900

    easier instance search

--- t/kb.lisp
+++ t/kb.lisp
@@ -7,7 +7,6 @@
        (nutritions (
 		     (a DietaryFiber with (weight ((a WeightValue with (num (1.6)) (unit (*MilliGram))))))
 		     (a Sugars with (weight ((a WeightValue with (num (825)) (unit (*MilliGram))))))
-		     (a Omega6FattyAcid with (weight ((a WeightValue with (num (825)) (unit (*MilliGram))))))
 		     (a SaturatedFat with (weight ((a WeightValue with (num (2.4)) (unit (*Gram))))))
 		     (a MonounsaturatedFat with (weight ((a WeightValue with (num (4.3)) (unit (*Gram))))))
 		     (a PolysaturatedFat with (weight ((a WeightValue with (num (0.9)) (unit (*Gram))))))
@@ -18,4 +17,4 @@
 
 (print (km-unique0 '#$(make-sentence (:seq "a cake have cooked and it's total weight is " (the sum of (the bag of (the num of(the weight of (the nutritions of *cake))))) *Gram "nospace" "."))))
 
-(print (km-unique0 '#$(make-sentence (:seq "It's weight of" Sugars "is" (the text of (the weight of (the first of (the nutritions of *cake))))))))
+(print (km-unique0 '#$(make-sentence (:seq "It's weight of" Sugars "is" (the text of (the weight of (the Sugars nutritions of *cake)))))))
