f62015113071e974f4bd620bb68344360d28a004
CH.Idea <sbw228@gmail.com>
2015-10-25 15:12:53 +0900

    person age sample

--- t/kb.lisp
+++ t/kb.lisp
@@ -4,7 +4,7 @@
 
 (km0 '#$(*cake has
        (instance-of (Food))
-       (nutritions (
+       (nutritions ((:bag
 		     (a DietaryFiber with (weight ((a WeightValue with (num (1.6)) (unit (*MilliGram))))))
 		     (a Sugars with (weight ((a WeightValue with (num (825)) (unit (*MilliGram))))))
 		     (a SaturatedFat with (weight ((a WeightValue with (num (2.4)) (unit (*Gram))))))
@@ -12,9 +12,16 @@
 		     (a PolysaturatedFat with (weight ((a WeightValue with (num (0.9)) (unit (*Gram))))))
 		     (a Omega3FattyAcid with (weight ((a WeightValue with (num (86.5)) (unit (*MilliGram))))))
 		     (a Omega6FattyAcid with (weight ((a WeightValue with (num (825)) (unit (*MilliGram))))))
-		     ))
+		     )))
        ))
+(km0 '#$(*me has
+	     (instance-of (Person))
+	     (gender (*Male))
+	     (age (0.2))
+	     (|daily taken calorie| ((a EnergyValue)))))
 
 (print (km-unique0 '#$(make-sentence (:seq "a cake have cooked and it's total weight is " (the sum of (the bag of (the num of(the weight of (the nutritions of *cake))))) *Gram "nospace" "."))))
 
 (print (km-unique0 '#$(make-sentence (:seq "It's weight of" Sugars "is" (the text of (the weight of (the Sugars nutritions of *cake)))))))
+
+(print (km-unique0 '#$(make-sentence (:seq "My" Gender "is" (the gender of *me) "and my daily needed calorie is" (the |daily needed calorie| of *me)))))
