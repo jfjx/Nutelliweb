e9e9741033758da1a4a939b76c7ca5e6f6998192
Chainbrain <chainbrain1@gmail.com>
2015-11-05 06:51:41 +0900

    implemented basic structure of Nutelliweb

--- webapp/src/web.lisp
+++ webapp/src/web.lisp
@@ -23,8 +23,38 @@
 ;;
 ;; Routing rules
 
-(defroute "/" ()
-  (render #P"index.html"))
+(defroute ("/" :method :GET) ()
+	(if (gethash :person *session*)
+		(render #P"redirect.html" '(("url" . "/dashboard"))) ;; Redirect to dashboard if there's personal info in session
+		(render #P"front.html")
+	))
+  
+(defroute ("/" :method :POST) (&key _parsed)
+	(setf (gethash :person *session*) (cdr (assoc "person" _parsed :test #'string=)))
+	(render #P"redirect.html" '(("url" . "/dashboard"))))
+	;;(render #P"front.html" (list "person" (gethash :person *session*))))
+	;;(format nil "~S" (list "person" (cdr(assoc "person" _parsed :test #'string=))))
+	;;(render #P"front.html" '( person ( ("name" . "test") ("gender" . "male") ("age" . "1") ("weight" . "2") ("height" . "3") ) )))
+  
+(defroute "/dashboard" ()
+	(unless (gethash :person *session*) (throw-code 403))
+	(render #P"dashboard.html"))
+
+(defroute "/setting" ()
+	(unless (gethash :person *session*) (throw-code 403))
+	(render #P"setting.html" (list "person" (gethash :person *session*))))
+
+(defroute "/recommended" ()
+	(unless (gethash :person *session*) (throw-code 403))
+	(render #P"recommended.html"))
+
+(defroute "/dishes" ()
+	(unless (gethash :person *session*) (throw-code 403))
+	(render #P"dishes.html"))
+
+(defroute "/nutrient" ()
+	(unless (gethash :person *session*) (throw-code 403))
+	(render #P"nutrient.html"))
 
 ;;
 ;; Error pages
