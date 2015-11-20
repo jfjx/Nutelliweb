2dedd0e2223df6c24e35bb6fabacb0c9424287f5
CH.Idea <sbw228@gmail.com>
2015-11-20 09:49:59 +0900

    webbrowser autostart on main.lisp

--- t/kb.lisp
+++ t/kb.lisp
@@ -1,6 +1,7 @@
 ;;; Init
 (load "lib/km-2-5-33.fasl")
 (km '(untrace))
+(km0 '(nocomments))
 (km0 '(SETQ *OUTPUT-PRECISION* NIL)) ;remove rounding up of floating points
 (km0 '(unload-kb))
 (km0 '(load-kb "kb/Nutelliweb.kb")) ;load a chemical nutrition kb
